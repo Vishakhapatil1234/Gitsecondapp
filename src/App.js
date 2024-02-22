41e7b16457621d27687ae4716fd665cc9d0bfb10
Vishakhapatil1234 <rvis4448@gmail.com>
2024-02-22 11:19:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import UserInfo from "./components/UserInfo";
+
+
+function App() {
+  return (
+    <>
+      
+      <UserInfo/>
+    </>
+  );
+}
+
+export default App;
